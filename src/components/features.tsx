2572498da4a5897a9bdd7694355bf63db6b90a96
lybenson <gwanbit@gmail.com>
2024-07-04 15:27:31 +0800

    feat: customize sidebar

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -1,6 +1,6 @@
 const features = [
   {
-    icon: '/img/custom.svg',
+    icon: '/img/react.svg',
     title: 'Powered by React',
     description: 'Use react to bring faster access speed to your website.'
   },
@@ -25,7 +25,10 @@ export default function Features() {
   return (
     <div className='container m-auto mb-20 grid grid-cols-1 gap-4 px-4 md:grid-cols-2 lg:grid-cols-4'>
       {features.map((feature) => (
-        <div className='rounded-md bg-gray-100 p-4'>
+        <div
+          className='rounded-md bg-gray-100 p-4'
+          key={feature.title}
+        >
           <img
             className='flex h-12 w-12 items-center justify-center rounded-md bg-slate-300 p-2'
             src={feature.icon}
--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   const year = new Date().getFullYear()
   return (
-    <footer className='flex h-24 items-center justify-center border-t text-black'>
+    <footer className='flex h-0 items-center justify-center border-t text-black dark:text-white'>
       Copyright © {year} heapup
     </footer>
   )
